e2eff3e337aa6beb7732f4fb7db92a345b418060
mcyoo <64882430+mcyoo@users.noreply.github.com>
2021-09-20 18:03:32 +0900

    exhibition#1

--- src/components/collection4.js
+++ src/components/collection4.js
@@ -1,48 +1,295 @@
-// src/components/home.js
-
-import React from 'react';
+import React from "react";
 import Slider from "react-slick";
+import "../assets/App.css";
+import jcbc_logo from "../assets/img/logo.jpg";
+import collection1 from "../assets/img/collection1.png";
+import collection2 from "../assets/img/collection2.JPG";
+import collection4 from "../assets/img/collection4.png";
 
 class Collection4 extends React.Component {
+  state = {
+    isLoading: true,
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 1500);
+  }
+
   render() {
+    const { isLoading } = this.state;
+
     const settings = {
+      adaptiveHeight: true,
       dots: true,
+      arrows: false,
       infinite: true,
-      speed: 500,
+      speed: 400,
       slidesToShow: 1,
       slidesToScroll: 1,
-      adaptiveHeight: true,
-      initialSlide: 4,
-      arrows: false,
-      };
-    return(
-        <div>
-        <h2> Single Item</h2>
-        <Slider {...settings}>
-          <div>
-            <h3>0</h3>
+      swipeToSlide: true,
+      initialSlide: 3,
+      beforeChange: () => {
+        window.scrollTo({ top: 0, left: 0 });
+      },
+      appendDots: (dots) => (
+        <div
+          style={{
+            position: "fixed",
+            padding: "10px",
+            backgroundColor: "white",
+            textAlign: "center",
+            borderTop: "1px solid #e2e8f0",
+            marginBottom: "25px",
+          }}
+        >
+          <div style={{ flex: 1 }}> {dots} </div>
+        </div>
+      ),
+      customPaging: (i) => {
+        if (i === 0) {
+          return (
+            <div style={{ flex: 1 }}>
+              <img
+                className="w-12 lg:w-16"
+                alt="collection1"
+                src={collection1}
+              />
+            </div>
+          );
+        }
+        if (i === 1) {
+          return (
+            <div style={{ flex: 1 }}>
+              <img
+                className="w-12 lg:w-16"
+                alt="collection2"
+                src={collection2}
+              />
+            </div>
+          );
+        }
+        if (i === 2) {
+          return (
+            <div style={{ flex: 1 }}>
+              <img
+                className="w-12 lg:w-16"
+                alt="collection4"
+                src={collection4}
+              />
+            </div>
+          );
+        }
+      },
+    };
+
+    return (
+      <div className="">
+        {isLoading ? (
+          <div className="w-screen overflow-x-hidden">
+            <div className="pt-0">
+              <div className="px-2">
+                <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-2xl">
+                  <div className="flex items-center justify-center">
+                    <div className="bg-white p-4 rounded-md">
+                      <div className="w-2/3 h-6 bg-gray-200 rounded-full animate-pulse mb-4"></div>
+                      <div className="w-96 h-48 bg-gray-200 animate-pulse"></div>
+                      <div className="mt-8 h-32 w-full space-y-3">
+                        <div className="w-20 h-6 bg-gray-200 rounded-full animate-pulse"></div>
+                        <div className="w-full h-4 bg-gray-200 rounded-full animate-pulse"></div>
+                        <div className="w-full h-4 bg-gray-200 rounded-full animate-pulse"></div>
+                        <div className="w-1/2 h-4 bg-gray-200 rounded-full animate-pulse"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <h3>1</h3>
+        ) : (
+          <div className="">
+            <Slider {...settings}>
+              <div className="w-screen overflow-x-hidden">
+                <div className="pt-0">
+                  <div className="px-2">
+                    <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-2xl">
+                      <div className="flex">
+                        <div className="w-full">
+                          <div className="flex justify-between items-center p-3">
+                            <div className="flex flex-row items-center">
+                              <img
+                                src={jcbc_logo}
+                                className="rounded-full"
+                                width="40"
+                                alt="jcbc_logo"
+                              />
+                              <div className="flex flex-row items-center ml-2">
+                                {" "}
+                                <span className="font-bold mr-1 text-sm md:text-lg">
+                                  jejucleanboysclub
+                                </span>
+                                <small className="h-1 w-1 bg-gray-300 rounded-full mr-1 mt-1"></small>{" "}
+                              </div>
+                            </div>
+                            <div className="pr-2">
+                              {" "}
+                              <i className="fa fa-ellipsis-h text-gray-400 hover:cursor-pointer hover:text-gray-600"></i>
+                            </div>
+                          </div>
+                          <div>
+                            <img
+                              src={collection1}
+                              className="w-full h-60 md:h-80"
+                              alt="collection1"
+                            />
+                          </div>
+                          <div className="p-4 flex justify-between items-center">
+                            <div className="flex flex-row items-center">
+                              <i className="fa fa-heart-o mr-2 fa-1x hover:text-gray-600"></i>
+                              <i className="fa fa-comment-o mr-2 fa-1x hover:text-gray-600"></i>
+                              <i className="fa fa-send-o mr-2 fa-1x hover:text-gray-600"></i>
+                            </div>
+                            <div>
+                              <i className="fa fa-bookmark-o fa-1x hover:text-gray-600"></i>
+                            </div>
+                          </div>
+                          <div className="mx-4 mb-4">
+                            <span className="text-sm">
+                              해안가에 흰색 트레이가 버려져 있다. 가져와 그
+                              트레이에 예쁜 꽃을 심었다. 보기 좋았다.
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-screen overflow-x-hidden">
+                <div className="pt-0">
+                  <div className="px-2">
+                    <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-2xl">
+                      <div className="flex">
+                        <div className="w-full">
+                          <div className="flex justify-between items-center p-3">
+                            <div className="flex flex-row items-center">
+                              <img
+                                src={jcbc_logo}
+                                className="rounded-full"
+                                width="40"
+                                alt="jcbc_logo"
+                              />
+                              <div className="flex flex-row items-center ml-2">
+                                {" "}
+                                <span className="font-bold mr-1 text-sm md:text-lg">
+                                  jejucleanboysclub
+                                </span>
+                                <small className="h-1 w-1 bg-gray-300 rounded-full mr-1 mt-1"></small>{" "}
+                              </div>
+                            </div>
+                            <div className="pr-2">
+                              {" "}
+                              <i className="fa fa-ellipsis-h text-gray-400 hover:cursor-pointer hover:text-gray-600"></i>
+                            </div>
+                          </div>
+                          <div>
+                            <img
+                              src={collection2}
+                              className="w-full h-60 md:h-80"
+                              alt="collection2"
+                            />
+                          </div>
+                          <div className="p-4 flex justify-between items-center">
+                            <div className="flex flex-row items-center">
+                              <i className="fa fa-heart-o mr-2 fa-1x hover:text-gray-600"></i>
+                              <i className="fa fa-comment-o mr-2 fa-1x hover:text-gray-600"></i>
+                              <i className="fa fa-send-o mr-2 fa-1x hover:text-gray-600"></i>
+                            </div>
+                            <div>
+                              <i className="fa fa-bookmark-o fa-1x hover:text-gray-600"></i>
+                            </div>
+                          </div>
+                          <div className="mx-4 mb-4">
+                            <span className="text-sm">
+                              쓰레기를 줍다보면 다양한 색깔의 부표들이 버려져
+                              있다. 색깔 별로 모아 걸어두었다. 썩 괜찮아 보였다.
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-screen overflow-x-hidden">
+                <div className="pt-0">
+                  <div className="px-2">
+                    <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-2xl">
+                      <div className="flex">
+                        <div className="w-full">
+                          <div className="flex justify-between items-center p-3">
+                            <div className="flex flex-row items-center">
+                              <img
+                                src={jcbc_logo}
+                                className="rounded-full"
+                                width="40"
+                                alt="jcbc_logo"
+                              />
+                              <div className="flex flex-row items-center ml-2">
+                                {" "}
+                                <span className="font-bold mr-1 text-sm md:text-lg">
+                                  jejucleanboysclub
+                                </span>
+                                <small className="h-1 w-1 bg-gray-300 rounded-full mr-1 mt-1"></small>{" "}
+                              </div>
+                            </div>
+                            <div className="pr-2">
+                              {" "}
+                              <i className="fa fa-ellipsis-h text-gray-400 hover:cursor-pointer hover:text-gray-600"></i>
+                            </div>
+                          </div>
+                          <div>
+                            <img
+                              src={collection4}
+                              className="w-full h-60 md:h-80"
+                              alt="collection4"
+                            />
+                          </div>
+                          <div className="p-4 flex justify-between items-center">
+                            <div className="flex flex-row items-center">
+                              <i className="fa fa-heart-o mr-2 fa-1x hover:text-gray-600"></i>
+                              <i className="fa fa-comment-o mr-2 fa-1x hover:text-gray-600"></i>
+                              <i className="fa fa-send-o mr-2 fa-1x hover:text-gray-600"></i>
+                            </div>
+                            <div>
+                              <i className="fa fa-bookmark-o fa-1x hover:text-gray-600"></i>
+                            </div>
+                          </div>
+                          <div className="mx-4 mb-4">
+                            <span className="text-sm">
+                              다른 나라 언어로 적혀있는 라이터들을 모아서 갈라진
+                              돌 틈 사이로 두었더니 예쁘다..!
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Slider>
+            <div className="flex-col md:text-lg text-sm text-right mr-5 mb-24 border-t border-gray-400 md:mx-48 mx-4 mt-8">
+              <div className="mt-4">문의</div>
+              <div>📧 mcyoo247@gmail.com</div>
+              <div>📱 010-4737-4115</div>
+            </div>
           </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-        </Slider>
+        )}
       </div>
-    )
+    );
   }
 }
-
-export default Collection4
-
-// 이와 같은 방식으로 Community, Mypage 컴포넌트도 생성
+export default Collection4;
