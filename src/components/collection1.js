d9c24185ac7456ba483eb6ceaedf03d334ccf2b8
mcyoo <64882430+mcyoo@users.noreply.github.com>
2021-09-21 23:00:13 +0900

    exhibition#3

--- src/components/collection1.js
+++ src/components/collection1.js
@@ -5,6 +5,9 @@ import jcbc_logo from "../assets/img/logo.jpg";
 import collection1 from "../assets/img/collection1.png";
 import collection2 from "../assets/img/collection2.JPG";
 import collection4 from "../assets/img/collection4.png";
+import throwball from "../assets/video/throw.MOV";
+import miniplay from "../assets/video/miniplay.MOV";
+import together from "../assets/video/together.MOV";
 
 class Collection1 extends React.Component {
   state = {
@@ -78,6 +81,12 @@ class Collection1 extends React.Component {
             </div>
           );
         }
+        if (i === 3) {
+          return <div style={{ flex: 1 }}></div>;
+        }
+        if (i === 4) {
+          return <div style={{ flex: 1 }}></div>;
+        }
       },
     };
 
@@ -91,7 +100,7 @@ class Collection1 extends React.Component {
                   <div className="flex items-center justify-center">
                     <div className="bg-white p-4 rounded-md">
                       <div className="w-2/3 h-6 bg-gray-200 rounded-full animate-pulse mb-4"></div>
-                      <div className="w-96 h-48 bg-gray-200 animate-pulse"></div>
+                      <div className="w-72 md:w-96 h-48 bg-gray-200 animate-pulse"></div>
                       <div className="mt-8 h-32 w-full space-y-3">
                         <div className="w-20 h-6 bg-gray-200 rounded-full animate-pulse"></div>
                         <div className="w-full h-4 bg-gray-200 rounded-full animate-pulse"></div>
@@ -151,6 +160,12 @@ class Collection1 extends React.Component {
                               <i className="fa fa-bookmark-o fa-1x hover:text-gray-600"></i>
                             </div>
                           </div>
+                          <div className="mx-4 mb-4 text-sm">
+                            <span className="text-sm font-bold">
+                              플라스틱 바스켓 화분
+                            </span>
+                            , 2021.09
+                          </div>
                           <div className="mx-4 mb-4">
                             <span className="text-sm">
                               해안가에 흰색 트레이가 버려져 있다. 가져와 그
@@ -207,6 +222,12 @@ class Collection1 extends React.Component {
                             <div>
                               <i className="fa fa-bookmark-o fa-1x hover:text-gray-600"></i>
                             </div>
+                          </div>
+                          <div className="mx-4 mb-4 text-sm">
+                            <span className="text-sm font-bold">
+                              크리스마스 트리
+                            </span>
+                            , 2021.09
                           </div>
                           <div className="mx-4 mb-4">
                             <span className="text-sm">
@@ -265,6 +286,12 @@ class Collection1 extends React.Component {
                               <i className="fa fa-bookmark-o fa-1x hover:text-gray-600"></i>
                             </div>
                           </div>
+                          <div className="mx-4 mb-4 text-sm">
+                            <span className="text-sm font-bold">
+                              돌틈에 라이터
+                            </span>
+                            , 2021.09
+                          </div>
                           <div className="mx-4 mb-4">
                             <span className="text-sm">
                               다른 나라 언어로 적혀있는 라이터들을 모아서 갈라진
@@ -277,11 +304,132 @@ class Collection1 extends React.Component {
                   </div>
                 </div>
               </div>
+
+              <div className="w-screen overflow-x-hidden">
+                <div className="pt-0">
+                  <div className="px-2">
+                    <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-2xl">
+                      <div className="flex">
+                        <div className="w-full">
+                          <div className="flex justify-between items-center p-3">
+                            <div className="flex flex-row items-center">
+                              <img
+                                src={jcbc_logo}
+                                className="rounded-full"
+                                width="40"
+                                alt="jcbc_logo"
+                              />
+                              <div className="flex flex-row items-center ml-2">
+                                {" "}
+                                <span className="font-bold mr-1 text-sm md:text-lg">
+                                  jejucleanboysclub
+                                </span>
+                                <small className="h-1 w-1 bg-gray-300 rounded-full mr-1 mt-1"></small>{" "}
+                              </div>
+                            </div>
+                            <div className="pr-2">
+                              {" "}
+                              <i className="fa fa-ellipsis-h text-gray-400 hover:cursor-pointer hover:text-gray-600"></i>
+                            </div>
+                          </div>
+                          <div>
+                            <video
+                              autoPlay="autoplay"
+                              muted="muted"
+                              controls
+                              loop
+                              preload="auto"
+                            >
+                              <source src={throwball}></source>
+                            </video>
+                          </div>
+                          <div className="p-4 flex justify-between items-center">
+                            <div className="flex flex-row items-center">
+                              <i className="fa fa-heart-o mr-2 fa-1x hover:text-gray-600"></i>
+                              <i className="fa fa-comment-o mr-2 fa-1x hover:text-gray-600"></i>
+                              <i className="fa fa-send-o mr-2 fa-1x hover:text-gray-600"></i>
+                            </div>
+                            <div>
+                              <i className="fa fa-bookmark-o fa-1x hover:text-gray-600"></i>
+                            </div>
+                          </div>
+                          <div className="mx-4 mb-4">
+                            <span className="text-sm">
+                              저희는 가끔씩 이렇게 놉니다. 부표를 던지구요.
+                              최대한 멀리요.
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-screen overflow-x-hidden">
+                <div className="pt-0">
+                  <div className="px-2">
+                    <div className="max-w-md mx-auto bg-white shadow-lg rounded-md overflow-hidden md:max-w-2xl">
+                      <div className="flex">
+                        <div className="w-full">
+                          <div className="flex justify-between items-center p-3">
+                            <div className="flex flex-row items-center">
+                              <img
+                                src={jcbc_logo}
+                                className="rounded-full"
+                                width="40"
+                                alt="jcbc_logo"
+                              />
+                              <div className="flex flex-row items-center ml-2">
+                                {" "}
+                                <span className="font-bold mr-1 text-sm md:text-lg">
+                                  jejucleanboysclub
+                                </span>
+                                <small className="h-1 w-1 bg-gray-300 rounded-full mr-1 mt-1"></small>{" "}
+                              </div>
+                            </div>
+                            <div className="pr-2">
+                              {" "}
+                              <i className="fa fa-ellipsis-h text-gray-400 hover:cursor-pointer hover:text-gray-600"></i>
+                            </div>
+                          </div>
+                          <div>
+                            <video
+                              autoPlay="autoplay"
+                              muted="muted"
+                              controls
+                              loop
+                              preload="auto"
+                              className="w-full h-full"
+                            >
+                              <source src={miniplay}></source>
+                            </video>
+                          </div>
+                          <div className="p-4 flex justify-between items-center">
+                            <div className="flex flex-row items-center">
+                              <i className="fa fa-heart-o mr-2 fa-1x hover:text-gray-600"></i>
+                              <i className="fa fa-comment-o mr-2 fa-1x hover:text-gray-600"></i>
+                              <i className="fa fa-send-o mr-2 fa-1x hover:text-gray-600"></i>
+                            </div>
+                            <div>
+                              <i className="fa fa-bookmark-o fa-1x hover:text-gray-600"></i>
+                            </div>
+                          </div>
+                          <div className="mx-4 mb-4">
+                            <span className="text-sm">드럼도 치고요.</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </Slider>
             <div className="flex-col md:text-lg text-sm text-right mr-5 mb-24 border-t border-gray-400 md:mx-48 mx-4 mt-8">
-              <div className="mt-4">문의</div>
+              <div className="mt-4">제주클린보이즈클럽 작품 전시회</div>
+              <div>2021.10 ~ 2021.11</div>
+              <div>고내리 337-2 올레16코스</div>
               <div>📧 mcyoo247@gmail.com</div>
-              <div>📱 010-4737-4115</div>
             </div>
           </div>
         )}
